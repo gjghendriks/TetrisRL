2b5c1256e1179116faefa77ae242be623679317b
gjghendriks <gjghendriks@gmail.com>
2020-01-12 15:06:19 +0100

    Cleaned up mlp.py

--- src/mlp.py
+++ src/mlp.py
@@ -12,7 +12,7 @@ import random
 
 # defining params
 learning_rate = 0.1
-training_epochs = 1000
+training_epochs = 10
 batch_size = 1
 display_step = 1
 exploration_rate = 0.1
@@ -23,8 +23,6 @@ num_classes = 1
 
 # suppress warnings
 tf.get_logger().setLevel('ERROR')
-
-
 
 
 def compile_model():
@@ -59,122 +57,122 @@ def find_max_index(predictions):
 
 	return max_value, max_index
 
-
-
-def train():
-
+def initialize_writer():
 	#init writer
 
 	filename = "outputs/mlp/"
 	filename += datetime.datetime.now().strftime('%c')
 	filename += "_output_scores_MLP" 
 	filename += ".txt"
-	with open(filename, mode='w') as csv_file:
-		print("Writing to file: " + filename)
-		csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-
-
-		model = compile_model()
-
-
-		for i in range(training_epochs):
-			weights = model.get_weights()
-			#print(weights)
-			#init previous prediction
-			prev_prediction = None
-			# init a new board
-			board = tetris.Tetris(constants.HORZBLOCKS,constants.VERTBLOCKS)
-			# init final score
-			final_score = 0
-
-			#generate 1000 next states, the model will probably die before that
-			for epoch in range(1000):
-
-				'''
-				generate all next possible states
-				with:
-				    blk_list:           		list of all the block
-				    score:              		the current score
-				    representation:     		the representation of the state
-				    formatted_representation: 	the representation that is formatted (normilized and put in an tf.Variable) 
-				'''
-				states = board.run()
-
-				# nextStates() returns False if the state is invalid (Game over)
-				if(not states):
-					#pygame.time.wait(100)
-					print("Training epoch #:{}\tFinal score :\t\t{}".format(i,final_score))
-					final_score = prev_score
-					break
-
-
-				#init predictions
-				predictions = []
-				for state in states:
-					# for each state predict the expected score
-					p = model.predict(state["formatted_representation"])
-					#save that prediction
-					predictions.append(p)
-				# explore
-				if random.random() < exploration_rate:
-					choice = random.choice(states)
-					board.setState(choice)
-					#breakpoint()
-					max_value = model.predict(choice['formatted_representation'])[0]
-					eplore = True
-					print("explore")
-				#normal
-				else:
-					max_value, max_index = find_max_index(predictions)
-					board.setState(states[max_index[0]])
-
-				#print("max = {} index = {}".format(max_value, max_index))
-				#print(predictions[max_index[0]][max_index[1]])
-
-
-				#set the boardstate to the best predicted next state
-				board.detect_line()
-				board.draw_board()
-
-
-				'''
-				back prop the new prediction
-					V(St) = V(St) + alpha*[Rt+1 + gamma * V(St+1) - V(St)]
-				Where:
-					V(St): 		prediction of the previous state 						: prev_prediction
-					alpha:		constant step-size parameter (always 1 in our case)
-					Rt+1:		Reward of the current state (current score)				: prev_score - state["score"]
-					gamma:		Learning rate 											: constants.DISCOUNT_RATE
-					V(St+1):	Prediction of the current state 						: max_value
-				'''
-
-				# only able to back propagate when t > 0 (after one state)
-				if prev_prediction:
-					#print("now backproping")
-					value = state["score"]- prev_score + constants.DISCOUNT_RATE * max_value
-					#print("value to be backpropagated: {}".format(value))
-					#print("Components: prev = {} score = {} score_diff = {} discount_rate = {}, max_value = {} max - prev = {}".format(prev_prediction, state['score'], state["score"] - prev_score, constants.DISCOUNT_RATE, max_value, max_value - prev_prediction))
-
-					#breakpoint()
-					model.fit(x=prev_input,
-						y=value,
-						verbose=0)
+	csv_file = open(filename, mode='w')
+	print("Writing to file: " + filename)
+	csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+	return csv_file, csv_writer
 
 
 
-				#update previous prediction
-				prev_prediction = max_value
-				#update previous score
-				prev_score = board.score
-				#update previous input
+def train():
+
+	csv_file, csv_writer = initialize_writer()
+
+	model = compile_model()
+
+
+	for epoch in range(training_epochs):
+		weights = model.get_weights()
+		#print(weights)
+		#init previous prediction
+		prev_prediction = None
+		# init a new board
+		board = tetris.Tetris(constants.HORZBLOCKS,constants.VERTBLOCKS)
+		# init final score
+		final_score = 0
+
+		#generate 1000 next states, the model will probably die before that
+		for block in range(1000):
+
+			states = board.run()
+
+			# board.run() returns False if the state is invalid (Game over)
+			if(not states):
+				#pygame.time.wait(100)
+				print("Training epoch #:{}\tFinal score :\t\t{}".format(epoch,final_score))
+				final_score = prev_score
+				break
+
+
+			#construct predictions
+			predictions = []
+			for state in states:
+				# for each state predict the expected score
+				p = model.predict(state["formatted_representation"])
+				#save that prediction
+				predictions.append(p)
+			# explore
+			if random.random() < exploration_rate:
+				choice = random.choice(states)
+				board.setState(choice)
+				#breakpoint()
+				max_value = model.predict(choice['formatted_representation'])[0]
+				eplored = True
+				print("explore")
+			#normal
+			else:
+				explored = False
+				max_value, max_index = find_max_index(predictions)
+				board.setState(states[max_index[0]])
+
+			#print("max = {} index = {}".format(max_value, max_index))
+			#print(predictions[max_index[0]][max_index[1]])
+
+
+			#set the boardstate to the best predicted next state
+			board.detect_line()
+			board.draw_board()
+
+
+			'''
+			back prop the new prediction
+				V(St) = V(St) + alpha*[Rt+1 + gamma * V(St+1) - V(St)]
+			Where:
+				V(St): 		prediction of the previous state 						: prev_prediction
+				alpha:		constant step-size parameter (always 1 in our case)
+				Rt+1:		Reward of the current state (current score)				: prev_score - state["score"]
+				gamma:		Learning rate 											: constants.DISCOUNT_RATE
+				V(St+1):	Prediction of the current state 						: max_value
+			'''
+			# only able to back propagate when t > 0 (after one state)
+			if prev_prediction:
+				value = state["score"]- prev_score + constants.DISCOUNT_RATE * max_value
+
+				model.fit(x=prev_input,
+					y=value,
+					verbose=0)
+
+
+
+			#update previous prediction
+			prev_prediction = max_value
+			#update previous score
+			prev_score = board.score
+			#update previous input
+			if explored:
+				prev_input = choice['formatted_representation']
+			else:
 				for j in range(len(states)):
 					if j == max_index[0]:
 						prev_input = states[j]["formatted_representation"]
 
 
 
-			#print("done with one epoch")
-			csv_writer.writerow([final_score])
+		csv_writer.writerow([final_score])
+
+	#breakpoint()
+	csv_file.close()
+	print("Closing")
+	raise SystemExit
+
+
 
 if __name__ == "__main__":
     train()
